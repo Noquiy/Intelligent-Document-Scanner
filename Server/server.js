ead55570e607797dd612872a3e88357049ae8ea2
Acruse <72349223+Noquiy@users.noreply.github.com>
2023-07-21 19:47:16 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const fs = require('fs');  // Add this line
+const { spawn } = require('child_process');  // Add this line
 
 const app = express();
 
+const dir = './uploads';
+
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './uploads');
+        cb(null, dir);
     },
     filename: function(req, file, cb){
         cb(null, Date.now() + '-' + file.originalname);
@@ -18,6 +26,18 @@ const upload = multer({storage: storage});
 app.use(cors());
 
 app.post('/upload', upload.single('photo'), function(req, res, next){
+    // Run your Python script here
+    const python = spawn('python', ['./uploads/main.py', req.file.path]);
+
+    python.stdout.on('data', function (data) {
+      console.log('Pipe data from python script ...');
+      console.log(data.toString());
+    });
+
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+    });
+
     res.send({
         message: 'File uploaded successfully!',
         file: req.file,
@@ -26,4 +46,4 @@ app.post('/upload', upload.single('photo'), function(req, res, next){
 
 app.listen(3000, function() {
     console.log('App running on port 3000')
-})
+});
